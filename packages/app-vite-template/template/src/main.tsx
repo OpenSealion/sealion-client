7b16291e7bee95ef69d634b84d34d3675fab8bdd
duxiaoqing <duxiaoqing@pjlab.org.cn>
2023-12-18 11:08:17 +0800

    feat: add change-page-gray

--- packages/app-vite-template/template/src/main.tsx
+++ packages/app-vite-template/template/src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import Mlog from '@utils/mlog';
+import '@config/change-page-gray';
 import App from './app';
 
 Mlog.init();
